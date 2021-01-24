5141d48dc13c624a6d10279ec7ea1ef481080cf6
stainlessray <raymondcoolmail@gmail.com>
2021-01-24 10:30:53 -0500

    added second test for hashmap put(), testing map with string key, as well as int key.

--- src/test/java/com/codedifferently/collections/hashmapuh/EntryTest.java
+++ src/test/java/com/codedifferently/collections/hashmapuh/EntryTest.java
@@ -6,19 +6,31 @@ import org.junit.Test;
 
 public class EntryTest<K, V> {
 
-    HashMapUh<Integer, String> map = new HashMapUh<>(32);
+    HashMapUh<Integer, String> mapIntStr = new HashMapUh<>(32);
+    HashMapUh<String, String> mapStrStr = new HashMapUh<>(32);
+
 
     @Before
     public void setUp() {
     }
 
-    //todo create tests
-    @Test
+    @Test // test put() with <int, string>
     public void testPut( ) {
         String expected = "textValue";
 
-        map.put(1, expected);
-        String actual = map.get(1);
+        mapIntStr.put(1, expected);
+        String actual = mapIntStr.get(1);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test // test put() with <string, string>
+    public void testPut2() {
+        String testKey = "testKey";
+        String expected = "testValue";
+
+        mapStrStr.put(testKey, expected);
+        String actual = mapStrStr.get(testKey);
 
         Assert.assertEquals(expected, actual);
     }

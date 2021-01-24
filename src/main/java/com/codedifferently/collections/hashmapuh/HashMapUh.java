9d7b1fba0dc1bb464319f3289b1ab771210fc17e
stainlessray <raymondcoolmail@gmail.com>
2021-01-24 10:02:39 -0500

    added javadoc notation to map code. added todos.

--- src/main/java/com/codedifferently/collections/hashmapuh/HashMapUh.java
+++ src/main/java/com/codedifferently/collections/hashmapuh/HashMapUh.java
@@ -2,16 +2,40 @@ package com.codedifferently.collections.hashmapuh;
 
 public class HashMapUh<K, V> {
 
+    // todo implement error throw/catches
+    //
+
+    /**
+     * field defining default size of map
+     */
     private int capacity = 16; //Initial default capacity
 
-    private Entry<K, V>[] table; //Array of Entry object
+    /**
+     * field defining an array of map entries
+     */
+    private Entry<K, V>[] table;
 
+    //todo
+    // create a method that takes in a map and capacity
+    // to scale the map up in size when outgrown
+    /**
+     * Use this method to create a new Entry[] table of [int capacity] size
+     * @param capacity refers to the size of the table to create
+     */
     public HashMapUh(int capacity){
         this.capacity = capacity;
         table = new Entry[capacity];
     }
 
+
+    /**
+     *
+     * @param key the map key of key:value
+     * @param value the value of key:value
+     */
     public void put(K key, V value){
+        // todo implement a check of size vs capacity
+        // and a method for scaling up when outgrown
         int index = index(key);
         Entry newEntry = new Entry(key, value, null);
         if(table[index] == null){
@@ -32,6 +56,12 @@ public class HashMapUh<K, V> {
         }
     }
 
+    /**
+     *  use this method to get the
+     *  value represented by the passed in key
+     * @param key the key to search for
+     * @return the value associated with the passed key
+     */
     public V get(K key){
         V value = null;
         int index = index(key);
@@ -46,6 +76,11 @@ public class HashMapUh<K, V> {
         return value;
     }
 
+    /**
+     * use this method tp
+     * remove the entry found at the passed key
+     * @param key the key to search for
+     */
     public void remove(K key){
         int index = index(key);
         Entry previous = null;
@@ -66,6 +101,10 @@ public class HashMapUh<K, V> {
         }
     }
 
+    /**
+     * use this method to display all
+     * entries in the map
+     */
     public void display(){
         for(int i = 0; i < capacity; i++){
             if(table[i] != null){
@@ -78,6 +117,14 @@ public class HashMapUh<K, V> {
         }
     }
 
+    /**
+     * use this method to create an index
+     * from the hashcode calculated from
+     * passed key
+     *
+     * @param key the key to hash
+     * @return
+     */
     private int index(K key){
         if(key == null){
             return 0;
--- src/test/java/com/codedifferently/collections/hashmapuh/HashMapUhTest.java
+++ src/test/java/com/codedifferently/collections/hashmapuh/HashMapUhTest.java
@@ -2,5 +2,8 @@ package com.codedifferently.collections.hashmapuh;
 
 public class HashMapUhTest<K, V> {
 
+    // todo
+    //  create tests for all public methods
+    // before any new code
 
 }
